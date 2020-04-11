db8e4212e8da5f9cd1d196b4974381deeb23098a
carlasanches <carlasanches8@gmail.com>
2020-04-11 12:11:03 -0300

    FunÃ§Ã£o meuNome
    Essa funÃ§Ã£o pede o nome do usuÃ¡rio e exibe na tela.

--- Projeto/src/projeto/Projeto.java
+++ Projeto/src/projeto/Projeto.java
@@ -5,20 +5,27 @@
  */
 package projeto;
 
+import java.util.Scanner;
+
 /**
  *
  * @author wando
  */
 public class Projeto {
+    
+    static void meuNome(String n){
+        
+        System.out.println("Hello " + n + "!");                
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        System.out.println("Hello World");
-        System.out.println("Hello Carla!");
-    }
-    
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Seu nome: ");
+        String n = ler.nextLine();
+        meuNome(n);        
+    }           
 }
